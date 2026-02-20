607aa241424beef8395ce083d06d569d70d3f3e4
dils <dils@berkeley.edu>
2026-02-20 04:56:21 -0800

    stuff

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.post("/debug/body", (req, res) => {
+  res.json({
+    contentType: req.headers["content-type"],
+    bodyType: typeof req.body,
+    body: req.body,
+  });
+});
 
 // --- session ---
 app.use(
